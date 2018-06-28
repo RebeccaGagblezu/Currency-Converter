12b31c4db38019612c0c594d215fd6c9478d6f75
RebeccaGagblezu <rebeccagagblezu@gmail.com>
2018-06-28 09:34:00 +0100

    Iinitial commit.
    Frontend created

---
+++ app.js
@@ -0,0 +1,68 @@
+const fromCurrency = document.getElementById("fromCurrency");
+const toCurrency = document.getElementById("toCurrency")
+
+window.addEventListener('load',e=>{
+    updateCurrency();
+    getCountries();
+});
+
+function updateCurrency(){
+  //  const res = await fetch('')
+}
+
+
+function getCountries(){
+    let myRequest = new Request(
+        'https://free.currencyconverterapi.com/api/v5/currencies',
+        {
+             'method': 'GET',
+        });
+    fetch(myRequest).then(function(response){
+          return response.json();
+//console.log(_currencyList);
+      //    fromCurrency.innerHTML =_currencyList.sources
+         // .map(src => `<option value="${src.id}">${src.currencyName}</option>`)
+
+
+    }).then(function(myJson) {
+        //console.log(myJson);
+       let _currencyObj = myJson.results;
+        let _currencyList = [];
+        for(_cur in _currencyObj){
+//            console.log(_currencyList[_cur].id);
+         //   console.log(_currencyList[_cur].currencyName);
+
+            _currencyList.push({
+                id:_currencyObj[_cur].id,
+                currencyName:_currencyObj[_cur].currencyName
+            })
+        }
+//console.log(_currencyList);
+        fromCurrency.innerHTML =_currencyList
+        .map(src => `<option value="${src.id}">${src.currencyName}</option>`)
+
+        toCurrency.innerHTML =_currencyList
+        .map(src => `<option value="${src.id}">${src.currencyName}</option>`)
+
+
+      });;
+}
+
+
+
+function convertCurrency() {
+    const fromCurr = document.getElementById('fromCurrency').value;
+    const toCurr = document.getElementById('toCurrency').value;
+    const amount = document.getElementById('curAmount').value;
+    let query = ` ${fromCurr}_${toCurr} `;
+    let myRequest = new Request(
+        'https://www.currencyconverterapi.com/api/v5/convert?q='
+            + query + '&compact=ultra',
+        {
+             'method': 'GET',
+        });
+        fetch(myRequest).then(response=>{console.log( response.json); })
+        // . then()
+}
+
+
